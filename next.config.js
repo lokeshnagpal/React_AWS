07aae29c8051db9ebf782d35a3a474a04f5708df
lokeshnagpal <lokeshnagpal86@gmail.com>
2025-09-20 15:00:08 +0530

    update next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Generate a fully static export into the `out/` directory
-  output: 'export',
-
+const nextConfig = {  
   // If you use next/image without a custom loader, keep this to avoid
   // relying on the default Image Optimization server during static export
   images: {
